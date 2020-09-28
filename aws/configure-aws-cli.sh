ac92d33f06c5e29d61633d1f00a82808851fc2f6
Tosin Akinosho <takinosh@redhat.com>
2020-09-28 09:19:33 -0400

    updating aws cli script

--- aws/configure-aws-cli.sh
+++ aws/configure-aws-cli.sh
@@ -1,46 +1,122 @@
 #!/bin/bash
-set -xe 
+#set -xe 
 
-if [ "$#" -ne 3 ];
-then 
-  echo "Please pass required arguments"
-  echo "USAGE: $0 aws_access_key_id aws_secret_access_key aws_region"
-  exit $1
-fi 
+
+# Print usage
+usage() {
+  echo -n "${0} [OPTION]
+
+ Options:
+  -i, --install     Install awscli latest binaries
+  -d, --delete      Remove awscli
+  -h, --help        Display this help and exit
+
+  USAGE: $0 -i aws_access_key_id aws_secret_access_key aws_region
+"
+
+}
+
 
 if [ "$EUID" -ne 0 ]
   then 
   RUN_SUDO="sudo"
 fi
 
+function install_aws_cli(){
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+  unzip awscliv2.zip
 
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
+  ${RUN_SUDO} ./aws/install
 
-${RUN_SUDO} ./aws/install
+  aws --version || exit 1
 
-aws --version || exit 1
+  if [ "$EUID" -ne 0 ]
+  then
+    rm -rf ${HOME}/awscli-bundle ${HOME}/awscli-bundle.zip 
+  else
+    rm -rf /root/awscli-bundle /root/awscli-bundle.zip 
+  fi
 
-if [ "$EUID" -ne 0 ]
-then
-  rm -rf ${HOME}/awscli-bundle ${HOME}/awscli-bundle.zip 
-else
-  rm -rf /root/awscli-bundle /root/awscli-bundle.zip 
-fi
+  export AWSKEYID=${1}
+  export AWSSECRETKEY=${2}
+  export REGION=${3}
 
-export AWSKEYID=${1}
-export AWSSECRETKEY=${2}
-export REGION=${3}
-
-mkdir -p $HOME/.aws
-cat  >$HOME/.aws/credentials<<EOF
+  mkdir -p $HOME/.aws
+  cat  >$HOME/.aws/credentials<<EOF
 [default]
 aws_access_key_id = ${AWSKEYID}
 aws_secret_access_key = ${AWSSECRETKEY}
 region = ${REGION}
 EOF
 
-cat $HOME/.aws/credentials
+  cat $HOME/.aws/credentials
 
-aws sts get-caller-identity || exit $?
+  aws sts get-caller-identity || exit $?
 
+}
+
+function delete_aws_cli(){
+  ${RUN_SUDO} rm -rf /usr/local/bin/aws
+
+  if [ "$EUID" -ne 0 ]
+  then
+    rm -rf ${HOME}/awscli-bundle ${HOME}/awscli-bundle.zip 
+  else
+    rm -rf /root/awscli-bundle /root/awscli-bundle.zip 
+  fi
+}
+
+optstring=v
+unset options
+while (($#)); do
+  case $1 in
+    # If option is of type -ab
+    -[!-]?*)
+      # Loop over each character starting with the second
+      for ((i=1; i < ${#1}; i++)); do
+        c=${1:i:1}
+
+        # Add current char to options
+        options+=("-$c")
+
+        # If option takes a required argument, and it's not the last char make
+        # the rest of the string its argument
+        if [[ $optstring = *"$c:"* && ${1:i+1} ]]; then
+          options+=("${1:i+1}")
+          break
+        fi
+      done
+      ;;
+
+    # If option is of type --foo=bar
+    --?*=*) options+=("${1%%=*}" "${1#*=}") ;;
+    # add --endopts for --
+    --) options+=(--endopts) ;;
+    # Otherwise, nothing special
+    *) options+=("$1") ;;
+  esac
+  shift
+done
+set -- "${options[@]}"
+unset options
+
+# Read the options and set stuff
+while [[ $1 = -?* ]]; do
+  case $1 in
+    -h|--help) usage >&2; break ;;
+    -i|--install)  shift; install_aws_cli $2 $3 $4;;
+    -d|--delete) shift; delete_aws_cli;;
+    --endopts) shift; break ;;
+    *) die "invalid option: '$1'." ;;
+  esac
+  shift
+done
+
+# Store the remaining part as arguments.
+args+=("$@")
+
+if [ -z $1 ];
+then
+  usage
+  exit 1
+fi
