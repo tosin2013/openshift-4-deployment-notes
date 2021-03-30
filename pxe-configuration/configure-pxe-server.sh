29c47d052d205e6ebde224d32fb664cc4acad868
Tosin Akinosho <takinosh@redhat.com>
2021-03-30 12:26:33 -0400

    Update configure-pxe-server.sh

--- pxe-configuration/configure-pxe-server.sh
+++ pxe-configuration/configure-pxe-server.sh
@@ -16,16 +16,16 @@ menu title **** OpenShift 4 PXE Boot Menu ****
 
 label Install CoreOS ${OC_VERSION} Bootstrap Node
  kernel /openshift4/${OC_VERSION}/rhcos-${BUILD_VERSION}-installer-kernel
- append ip=dhcp rd.neednet=1 coreos.inst.install_dev=vda console=tty0 console=ttyS0 coreos.inst=yes coreos.inst.image_url=http://${WEBSERVER}:8080/openshift4/${OC_VERSION}/images/rhcos-${OC_VERSION}-x86_64-metal-bios.raw.gz coreos.inst.ignition_url=http://${WEBSERVER}:8080/openshift4/${OC_VERSION}/ignitions/bootstrap.ign initrd=/openshift4/${OC_VERSION}/rhcos-${BUILD_VERSION}-installer-initramfs.img
+ append ip=dhcp rd.neednet=1 coreos.inst.install_dev=vda console=tty0 console=ttyS0 coreos.inst=yes coreos.inst.image_url=http://${WEBSERVER}:8080/openshift4/${OC_VERSION}/images/rhcos-metal.x86_64.raw.gz coreos.inst.ignition_url=http://${WEBSERVER}:8080/openshift4/${OC_VERSION}/ignitions/bootstrap.ign initrd=/openshift4/${OC_VERSION}/rhcos-live-initramfs.x86_64.img
 
 label Install CoreOS ${OC_VERSION} Master Node
  kernel /openshift4/${OC_VERSION}/rhcos-${BUILD_VERSION}-installer-kernel
- append ip=dhcp rd.neednet=1 coreos.inst.install_dev=vda console=tty0 console=ttyS0 coreos.inst=yes coreos.inst.image_url=http://${WEBSERVER}:8080/openshift4/${OC_VERSION}/images/rhcos-${OC_VERSION}-x86_64-metal-bios.raw.gz coreos.inst.ignition_url=http://${WEBSERVER}:8080/openshift4/${OC_VERSION}/ignitions/master.ign initrd=/openshift4/${OC_VERSION}/rhcos-${BUILD_VERSION}-installer-initramfs.img
+ append ip=dhcp rd.neednet=1 coreos.inst.install_dev=vda console=tty0 console=ttyS0 coreos.inst=yes coreos.inst.image_url=http://${WEBSERVER}:8080/openshift4/${OC_VERSION}/images/rhcos-metal.x86_64.raw.gz coreos.inst.ignition_url=http://${WEBSERVER}:8080/openshift4/${OC_VERSION}/ignitions/master.ign initrd=/openshift4/${OC_VERSION}/rhcos-live-initramfs.x86_64.img
 
 label Install CoreOS ${OC_VERSION} Worker Node
 
  kernel /openshift4/${OC_VERSION}/rhcos-${BUILD_VERSION}-installer-kernel
- append ip=dhcp rd.neednet=1 coreos.inst.install_dev=vda console=tty0 console=ttyS0 coreos.inst=yes coreos.inst.image_url=http://${WEBSERVER}:8080/openshift4/${OC_VERSION}/images/rhcos-${OC_VERSION}-x86_64-metal-bios.raw.gz coreos.inst.ignition_url=http://${WEBSERVER}:8080/openshift4/${OC_VERSION}/ignitions/worker.ign initrd=/openshift4/${OC_VERSION}/rhcos-${BUILD_VERSION}-installer-initramfs.img
+ append ip=dhcp rd.neednet=1 coreos.inst.install_dev=vda console=tty0 console=ttyS0 coreos.inst=yes coreos.inst.image_url=http://${WEBSERVER}:8080/openshift4/${OC_VERSION}/images/rhcos-metal.x86_64.raw.gz coreos.inst.ignition_url=http://${WEBSERVER}:8080/openshift4/${OC_VERSION}/ignitions/worker.ign initrd=/openshift4/${OC_VERSION}/rhcos-live-initramfs.x86_64.img
 EOF
 
 cat /var/lib/tftpboot/pxelinux.cfg/default
