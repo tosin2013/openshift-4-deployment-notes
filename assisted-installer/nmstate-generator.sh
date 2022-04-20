725f19fb45becc22feae43ad4cc7b71cfcf338e5
Tosin Akinosho <tosin.akinosho@gmail.com>
2022-04-19 22:07:46 -0400

    updating scripts

--- assisted-installer/nmstate-generator.sh
+++ assisted-installer/nmstate-generator.sh
@@ -26,16 +26,48 @@
           echo ${node} | base64 --decode | jq -r ${1}
         }
         echo "  Creating NMState config for $(_jq '.name')..."
-        dns_entries=${CLUSTER_NODE_NET_DNS_SERVERS[@]}
-        interface_name=$(_jq '.ipv4.iface')
-        static_ipv4_address=$(_jq '.ipv4.address')
-        static_ipv4_prefix=$(_jq '.ipv4.prefix')
-        auto_dns=${USE_AUTO_DNS}
-        echo  dns_entries=${dns_entries}
-        echo interface_name=${interface_name}
-        echo static_ipv4_address=${static_ipv4_address}
-        echo static_ipv4_prefix=${static_ipv4_prefix}
-        echo auto_dns=${USE_AUTO_DNS}
-        j2 network-templates/single-nic-static.j2
-        exit $?
+        
+        export auto_dns=${USE_AUTO_DNS}
+        export parse_dns_vars=$( IFS=,; printf '%s' "${CLUSTER_NODE_NET_DNS_SERVERS[*]}" )
+
+        if [ $MULTI_NETWORK  == false ];
+        then 
+          export interface_name=$(_jq '.ipv4.iface')
+          export static_ipv4_address=$(_jq '.ipv4.address')
+          export static_ipv4_prefix=$(_jq '.ipv4.prefix')
+          export static_ipv4_gateway=$(_jq '.ipv4.gateway')
+          export vlan_id=${VLAN_ID}
+
+          if [ $USE_DHCP == false ] && [ $USE_VLAN == false ]; # Single Network static IPS
+          then 
+            j2 network-templates/single-nic-static.j2 | sed '/^$/d' | tee ${CLUSTER_DIR}/$(_jq '.name').yaml
+          elif [ $USE_DHCP == true ] && [ $USE_VLAN == false ]; # Single Network VLAN NIC with Static IPS
+          then 
+            j2 network-templates/single-nic-dhcp.j2 | sed '/^$/d' | tee ${CLUSTER_DIR}/$(_jq '.name').yaml
+          fi
+          elif [ $USE_DHCP == false ] && [ $USE_VLAN == true ]; # Single Network VLAN NIC with Static IPS
+          then 
+            j2 network-templates/single-nic-vlan-static.j2 | sed '/^$/d' | tee ${CLUSTER_DIR}/$(_jq '.name').yaml
+          elif [ $USE_DHCP == true ] && [ $USE_VLAN == true ]; # DHCP Network VLAN NIC with Static IPS
+          then 
+            j2 network-templates/single-nic-vlan-dhcp.j2 | sed '/^$/d' | tee ${CLUSTER_DIR}/$(_jq '.name').yaml
+          fi
+        elif [ $MULTI_NETWORK == true ];
+        then 
+          export interface_one_name=$(_jq '.ipv4_int1.iface')
+          export interface_one_address=$(_jq '.ipv4_int1.address')
+          export interface_one_prefix=$(_jq '.ipv4_int1.prefix')
+          export interface_one_gateway=$(_jq '.ipv4_int1.gateway')
+          export interface_two_name=$(_jq '.ipv4_int2.iface')
+          export interface_two_address=$(_jq '.ipv4_int2.address')
+          export interface_two_prefix=$(_jq '.ipv4_int2.prefix')
+          if  [ $USE_DHCP == true ] && [ $USE_VLAN == false ];  # Multi Network DHCP
+          then 
+            j2 network-templates/mutli-nic-dhcp.j2 | sed '/^$/d' | tee ${CLUSTER_DIR}/$(_jq '.name').yaml 
+          elif [ $USE_DHCP == false ] && [ $USE_VLAN == false ]; # Multi Network Static IPs
+          then 
+            j2 network-templates/mutli-nic-static.j2 | sed '/^$/d' | tee ${CLUSTER_DIR}/$(_jq '.name').yaml 
+          fi
+        fi 
+
       done
