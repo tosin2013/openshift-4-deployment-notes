8e7afa18c7a2ce6864ca4ccb71be1b2c350eb58b
Tosin Akinosho <tosin.akinosho@gmail.com>
2022-07-11 10:35:56 -0400

    correcting download script and create kvm script

--- assisted-installer/download-iso.sh
+++ assisted-installer/download-iso.sh
@@ -10,23 +10,37 @@ sleep 15
 
 ## Download Discovery ISO
 
-## Get InfraEnv information
-INFRA_ENV=$(curl -s \
-  -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $ACTIVE_TOKEN" \
-  -L "${ASSISTED_SERVICE_V2_API}/infra-envs/$INFRAENV_ID")
-
-ISO_DOWNLOAD_LINK=$(echo $INFRA_ENV | jq -r '.download_url')
-
-curl \
-  -H "Authorization: Bearer $ACTIVE_TOKEN" \
-  -L "${ISO_DOWNLOAD_LINK}" \
-  -o ${CLUSTER_DIR}/ai-liveiso-$CLUSTER_ID.iso
 
 if [ ! -z ${NEW_CLUSTER_ID} ];
 then
-  mv ${CLUSTER_DIR}/ai-liveiso-$CLUSTER_ID.iso ${CLUSTER_DIR}/ai-liveiso-addhosts-$CLUSTER_ID.iso 
-  echo -e "\n===== Downloading Discovery ISO locally to ${CLUSTER_DIR}/ai-liveiso-addhosts-$CLUSTER_ID.iso ...\n"
+  #mv ${CLUSTER_DIR}/ai-liveiso-$CLUSTER_ID.iso ${CLUSTER_DIR}/ai-liveiso-addhosts-$CLUSTER_ID.iso 
+  #echo -e "\n===== Downloading Discovery ISO locally to ${CLUSTER_DIR}/ai-liveiso-addhosts-$NEW_CLUSTER_ID.iso ...\n"
+  ## Get InfraEnv information
+  INFRA_ENV=$(curl -s \
+    -H "Content-Type: application/json" \
+    -H "Authorization: Bearer $ACTIVE_TOKEN" \
+    -L "${ASSISTED_SERVICE_V2_API}/infra-envs/$INFRAENV_ID")
+
+  ISO_DOWNLOAD_LINK=$(echo $INFRA_ENV | jq -r '.download_url')
+
+  curl \
+    -H "Authorization: Bearer $ACTIVE_TOKEN" \
+    -L "${ISO_DOWNLOAD_LINK}" \
+    -o ${CLUSTER_DIR}/ai-liveiso-addhosts-$NEW_CLUSTER_ID.iso
+  echo -e "\n===== Downloading Discovery ISO locally to ${CLUSTER_DIR}/ai-liveiso-addhosts-$NEW_CLUSTER_ID.iso ...\n"
+
 else 
   echo -e "\n===== Downloading Discovery ISO locally to ${CLUSTER_DIR}/ai-liveiso-$CLUSTER_ID.iso ...\n"
+  ## Get InfraEnv information
+  INFRA_ENV=$(curl -s \
+    -H "Content-Type: application/json" \
+    -H "Authorization: Bearer $ACTIVE_TOKEN" \
+    -L "${ASSISTED_SERVICE_V2_API}/infra-envs/$INFRAENV_ID")
+
+  ISO_DOWNLOAD_LINK=$(echo $INFRA_ENV | jq -r '.download_url')
+
+  curl \
+    -H "Authorization: Bearer $ACTIVE_TOKEN" \
+    -L "${ISO_DOWNLOAD_LINK}" \
+    -o ${CLUSTER_DIR}/ai-liveiso-$CLUSTER_ID.iso
 fi
