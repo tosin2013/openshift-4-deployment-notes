36abddda1f8c2f6ca10f7f4c27ba3bf78b9135f6
Tosin Akinosho <takinosh@redhat.com>
2022-09-29 15:39:10 -0400

    adding new scripts

---
+++ disconnected-scripts/disconnected-oc-mirror.sh
@@ -0,0 +1,51 @@
+ #!/usr/bin/env bash
+# WIP 
+
+export OCP_RELEASE="4.11"
+export MERGED_PULLSECRET="merged-pull-secret.json"
+export OCP_PULLSECRET_AUTHFILE="~/${MERGED_PULLSECRET}"
+export REGISTRY_NAME=quay.ztp-pipelines.ocpincubator.com
+export REGISTRY_PORT=8443
+export LOCAL_REGISTRY=$REGISTRY_NAME:$REGISTRY_PORT
+export IMAGE_TAG=olm
+
+# Add extra registry keys
+sudo curl -o /etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-isv https://www.redhat.com/security/data/55A34A82.txt
+sudo jq ".transports.docker += {\"registry.redhat.io/redhat/certified-operator-index\": [{\"type\": \"signedBy\",\"keyType\": \"GPGKeys\",\"keyPath\": \"/etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-isv\"}], \"registry.redhat.io/redhat/community-operator-index\": [{\"type\": \"signedBy\",\"keyType\": \"GPGKeys\",\"keyPath\": \"/etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-isv\"}], \"registry.redhat.io/redhat/redhat-marketplace-operator-index\": [{\"type\": \"signedBy\",\"keyType\": \"GPGKeys\",\"keyPath\": \"/etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-isv\"}]}" < /etc/containers/policy.json > /etc/containers/policy.json.new
+sudo mv /etc/containers/policy.json.new /etc/containers/policy.json
+
+# Login registries
+REGISTRY_USER=init
+REGISTRY_PASSWORD=1C7cA65JEt2kilFyYgm9sI0G4Kjd3BQ8
+podman login -u $REGISTRY_USER -p $REGISTRY_PASSWORD $LOCAL_REGISTRY
+#podman login registry.redhat.io --authfile ${HOME}/openshift_pull.json
+REDHAT_CREDS=$(cat ~/${MERGED_PULLSECRET} | jq .auths.\"registry.redhat.io\".auth -r | base64 -d)
+RHN_USER=$(echo $REDHAT_CREDS | cut -d: -f1)
+RHN_PASSWORD=$(echo $REDHAT_CREDS | cut -d: -f2)
+podman login -u "$RHN_USER" -p "$RHN_PASSWORD" registry.redhat.io
+
+which oc-mirror >/dev/null 2>&1
+if [ "$?" != "0" ] ; then
+  #curl -OL https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/latest/oc-mirror.tar.gz
+  curl -OL https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable-4.10/oc-mirror.tar.gz
+  tar xvzf oc-mirror.tar.gz
+  sudo mv oc-mirror /usr/bin/oc-mirror
+  sudo chmod +x /usr/bin/oc-mirror
+  oc-mirror version
+fi
+
+mkdir -p ${HOME}/.docker
+cp -f ${HOME}/${MERGED_PULLSECRET} ${HOME}/.docker/config.json
+
+oc mirror init --registry ${LOCAL_REGISTRY}/openshift/oc-mirror > imageset-config.yaml 
+oc mirror --config=./imageset-config.yaml docker://${LOCAL_REGISTRY}/openshift
+
+rm -rf ${HOME}/oc-mirror-workspace || true
+oc-mirror --config ${HOME}/mirror-config.yaml docker://$LOCAL_REGISTRY
+oc apply -f ${HOME}/oc-mirror-workspace/results-*/imageContentSourcePolicy.yaml 2>/dev/null || cp ${HOME}/oc-mirror-workspace/results-*/imageContentSourcePolicy.yaml ${HOME}/manifests
+oc apply -f ${HOME}/oc-mirror-workspace/results-*/catalogSource* 2>/dev/null || cp ${HOME}/oc-mirror-workspace/results-*/catalogSource* ${HOME}/manifests
+
+
+ oc-mirror-workspace/operators.1664245965/manifests-redhat-operator-index
+
+  oc-mirror-workspace/operators.1664246234/manifests-redhat-operator-index
---
+++ disconnected-scripts/disconnected-olm.sh
@@ -0,0 +1,86 @@
+#!/bin/bash 
+set -xe 
+export INTERNAL_REGISTRY=registry.example.com
+export PULL_SECRET_JSON=~/pull-secret.json
+export LOCAL_SECRET_JSON=~/merged-pull-secret.json
+export OPENSHIFT_VERSION="4.10"
+export PORT=8443
+export LOCAL_REGISTRY=${INTERNAL_REGISTRY}:${PORT}
+export LOCAL_REPOSITORY=olm-mirror
+export TLS_VERIFY=false
+export EMAIL="admin@changeme.com"
+export PASSWORD="CHANGEME"
+export USERNAME="init"
+export AUTH="$(echo -n 'init:${PASSWORD}' | base64 -w0)" # in base 64
+
+if [ ! -f /usr/local/bin/grpcurl ];
+then 
+    curl -OL https://github.com/fullstorydev/grpcurl/releases/download/v1.8.6/grpcurl_1.8.6_linux_x86_64.tar.gz
+    tar -zxvf grpcurl_1.8.6_linux_x86_64.tar.gz
+    sudo mv grpcurl /usr/local/bin/
+fi
+
+if [ ! -f /usr/local/bin/opm ];
+then 
+    curl -OL https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable-${OPENSHIFT_VERSION}/opm-linux.tar.gz
+    tar -zxvf opm-linux.tar.gz
+    sudo mv opm /usr/local/bin 
+fi 
+
+function create_merge_secret(){
+    if [ -f ${PULL_SECRET_JSON} ];
+    then 
+
+        cat <<EOF > ~/reg-secret.txt
+"${LOCAL_REGISTRY}": {
+    "email":  "${EMAIL}",
+    "auth": "${AUTH}"
+}
+EOF
+
+        cat ${PULL_SECRET_JSON} |jq ".auths += {`cat ~/reg-secret.txt`}"|tr -d '[:space:]' > ${LOCAL_SECRET_JSON}
+    else
+        echo "${PULL_SECRET_JSON} not found please add"
+        echo "Plese go to https://console.redhat.com/openshift/install/"
+        exit 
+    fi  
+}
+
+oc patch OperatorHub cluster --type json \
+    -p '[{"op": "add", "path": "/spec/disableAllDefaultSources", "value": true}]'
+
+REDHAT_CREDS=$(cat ${PULL_SECRET_JSON} | jq .auths.\"registry.redhat.io\".auth -r | base64 -d)
+RHN_USER=$(echo $REDHAT_CREDS | cut -d: -f1)
+RHN_PASSWORD=$(echo $REDHAT_CREDS | cut -d: -f2)
+podman login -u "$RHN_USER" -p "$RHN_PASSWORD" registry.redhat.io
+
+podman  login ${LOCAL_REGISTRY}  -u ${USERNAME} -p ${PASSWORD}  --tls-verify=${TLS_VERIFY}
+
+podman run -p50051:50051 -d -it registry.redhat.io/redhat/redhat-operator-index:v${OPENSHIFT_VERSION}
+sleep 15s
+rm -rf packages.out saved-packages.log
+grpcurl -plaintext localhost:50051 api.Registry/ListPackages > packages.out
+
+cat packages.out | grep -E 'local-storage|odf-*|ocs|openshift-gitops-operator|advanced-cluster-management|ansible-automation-platform-operator|cincinnati-operator|klusterlet-product|mcg-operator|multicluster-engine|openshift-pipelines-operator-rh|quay-operator'  | awk '{print $2}' | tr '"' ' '  | sed 's/ //g' | tee -a saved-packages.log
+
+opm index prune \
+    -f registry.redhat.io/redhat/redhat-operator-index:v${OPENSHIFT_VERSION} \
+    -p $(cat saved-packages.log | paste -d ',' -s) \
+    -t ${LOCAL_REGISTRY}/${LOCAL_REPOSITORY}/redhat-operator-index:v${OPENSHIFT_VERSION}
+
+podman login --authfile ${LOCAL_SECRET_JSON} \
+  ${LOCAL_REGISTRY} \
+  --tls-verify=${TLS_VERIFY} 
+podman push ${LOCAL_REGISTRY}/${LOCAL_REPOSITORY}/redhat-operator-index:v${OPENSHIFT_VERSION}
+
+
+oc adm catalog mirror  ${LOCAL_REGISTRY}/${LOCAL_REPOSITORY}/redhat-operator-index:v${OPENSHIFT_VERSION}  ${LOCAL_REGISTRY}/${LOCAL_REPOSITORY} -a ${LOCAL_SECRET_JSON}
+
+cd manifests-redhat-operator-index-*   
+oc create -f imageContentSourcePolicy.yaml
+oc create -f catalogSource.yaml 
+oc get pods -n openshift-marketplace
+
+sudo podman stop $(sudo podman ps -a | grep redhat-operator-index | awk '{print $1}')
+sudo podman rm  $(sudo podman ps -a | grep redhat-operator-index | awk '{print $1}')
+rm -rf manifests-redhat-operator-index-*
